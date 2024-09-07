4aa83dd7215ab12da0a076df041646210f4a4bf5
alicia <112639409+alicia4645@users.noreply.github.com>
2024-09-07 21:57:56 +0100

    created a 16x16 grid of square divs

---
+++ etch-a-sketch.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container")
+
+function createGrids(){
+    let width = container.clientWidth / 16;
+    console.log(width)
+    let height = container.clientHeight / 16;
+    let gridSize = 16*16
+    for(let i=0; i<gridSize; i++){
+        const grid = document.createElement("div")
+        grid.classList.add("grid")
+        grid.style.width = `${width}px`
+        grid.style.height = `${height}px`
+        container.appendChild(grid)
+    }
+}
+
+createGrids();
