9f2f617e42a04ac3bfb3181b9f0efe67c449e90c
alicia <112639409+alicia4645@users.noreply.github.com>
2024-09-08 14:10:46 +0100

    made size of grid change with user input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,16 @@
 const container = document.querySelector("#container")
 
-function createGrids(){
-    let width = container.clientWidth / 16;
-    console.log(width)
-    let height = container.clientHeight / 16;
-    let gridSize = 16*16
+const gridSize = document.querySelector("#size");
+
+function createGrids(size){
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
+
+    let width = container.clientWidth / size;
+    let height = container.clientHeight / size;
+    let gridSize = size*size
+    
     for(let i=0; i<gridSize; i++){
         const grid = document.createElement("div")
         grid.classList.add("grid")
@@ -12,14 +18,21 @@ function createGrids(){
         grid.style.height = `${height}px`
         container.appendChild(grid)
     }
+
+    for(const grid of container.children){
+        grid.addEventListener('mouseenter', () => {
+            grid.style.backgroundColor = "purple"
+            console.log(container.clientWidth)
+        })
+    }
+    
 }
 
-createGrids();
+createGrids(16);
 
-for(const grid of container.children){
-    grid.addEventListener('mouseenter', () => {
-        grid.style.backgroundColor = "purple"
-        console.log(container.clientWidth)
-    })
-}
 
+gridSize.addEventListener('keypress' , (event) => {
+    if(event.key === "Enter"){
+        createGrids(gridSize.value)
+    }
+})
